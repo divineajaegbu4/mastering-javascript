75c83551f12a595329b2e7f451fed6e71e1cfbec
Divine Ajaegbu <divineajaegbu4@gmail.com>
2025-09-30 16:50:52 +0100

    feat(variables): add swap two variables without a third variable with varibles name, age and admin

---
+++ introduction-to-javascript/javascript-fundamentals/variables.js
@@ -0,0 +1,27 @@
+// Task from javascript.info
+const name = "Divine";
+const admin = name;
+
+console.log(admin);
+
+// Swap two variables without a third variable
+
+let swapA = 10;
+
+let swapB = 20;
+
+swapA = swapB + swapA; // 30
+swapB = swapA - swapB; // 10
+swapA = swapA - swapB; // 20
+
+console.log(`swapA: ${swapA}`);
+console.log(`swapB: ${swapB}`);
+
+//  Create variables for your name and age, display them
+
+const myName = "Divine Ajaegbu";
+
+const myAge = 21;
+
+console.log(`My name is ${myName} and I am ${myAge} years old.`);
+
