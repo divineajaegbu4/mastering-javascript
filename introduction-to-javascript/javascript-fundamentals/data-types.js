660ed45f9287b7711fd71c2eef27786baffd350a
Divine Ajaegbu <divineajaegbu4@gmail.com>
2025-09-30 19:39:59 +0100

    feat(data-types): check if a number is integer and add concatenation of strings

---
+++ introduction-to-javascript/javascript-fundamentals/data-types.js
@@ -0,0 +1,14 @@
+// Check if a number is integer
+const checkInteger = typeof 42;
+
+console.log(checkInteger); // "number"
+
+// Concatenate two strings
+
+const firstName = "Divine";
+
+const lastName = "Ajaegbu";
+
+const fullName = `My name is ${firstName} ${lastName}.`;
+
+console.log(fullName);
